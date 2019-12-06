0c7f9d15ebf71dac29779c0550fbee8bc84bec7a
SAULO SOARES OLIVEIRA <saulosoares@outlook.com>
2019-12-06 18:28:36 -0200

    Initial commit.

---
+++ src/webparts/wpGetItems/components/WpGetItems.module.scss.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+require("./WpGetItems.module.css");
+const styles = {
+  wpGetItems: 'wpGetItems_34bd9a6d',
+  container: 'container_34bd9a6d',
+  row: 'row_34bd9a6d',
+  column: 'column_34bd9a6d',
+  'ms-Grid': 'ms-Grid_34bd9a6d',
+  title: 'title_34bd9a6d',
+  subTitle: 'subTitle_34bd9a6d',
+  description: 'description_34bd9a6d',
+  button: 'button_34bd9a6d',
+  label: 'label_34bd9a6d',
+  titulo: 'titulo_34bd9a6d',
+  'products-list': 'products-list_34bd9a6d',
+  'product-info': 'product-info_34bd9a6d',
+};
+
+export default styles;
+/* tslint:enable */
